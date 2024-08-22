d0f73c9a1ed50d7d6818db9b0c9c628e650b1247
Harshad Deshmukh <147603991+harshaadeshmukh@users.noreply.github.com>
2024-08-22 23:58:17 +0530

    Time: 2 ms (50.07%), Space: 7.5 MB (33.08%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        if(num==0)
+        {
+            return 1;
+        }
+        for(long i=1;i<=num;i*=2)
+        {
+            num^=i;
+        }
+        return num;
+    }
+};
